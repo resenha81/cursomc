fa2bce609430e4b0a81462246382ccf8b2f0a440
Wagner Cruz <wa.cruz81@gmail.com>
2023-08-27 16:14:12 -0300

    Projeto criado

---
+++ src/main/java/com/wagnercruz/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.wagnercruz.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wagnercruz/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,30 @@
+package com.wagnercruz.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wagnercruz.cursomc.domain.Categoria;
+import com.wagnercruz.cursomc.services.CategoriaService;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+
+}
