f261cafdeeb897b645eb898845da05c5d4b6b161
Wagner Cruz <wa.cruz81@gmail.com>
2023-08-28 15:26:01 -0300

    Produto e associacao uitos para muitos

--- src/main/java/com/wagnercruz/cursomc/domain/Categoria.java
+++ src/main/java/com/wagnercruz/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.wagnercruz.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -18,6 +21,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria () {
 		
@@ -45,6 +51,14 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -61,6 +75,8 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 
--- src/main/java/com/wagnercruz/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/wagnercruz/cursomc/repositories/ProdutoRepository.java
@@ -3,10 +3,9 @@ package com.wagnercruz.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.wagnercruz.cursomc.domain.Categoria;
-
+import com.wagnercruz.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 }
