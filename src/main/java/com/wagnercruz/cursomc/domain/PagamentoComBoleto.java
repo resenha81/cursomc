66c635edad31cfdd88e543991a273d0b7a7ecdeb
Wagner Cruz <wa.cruz81@gmail.com>
2023-08-29 17:21:36 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/wagnercruz/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,45 @@
+package com.wagnercruz.cursomc.domain;
+
+import java.util.Date;
+
+import com.wagnercruz.cursomc.domain.enums.EstadoPagamento;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataVencimento;
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {
+		
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.setDataPagamento(dataPagamento);
+		this.setDataVencimento(dataVencimento);
+
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	
+	
+
+}
